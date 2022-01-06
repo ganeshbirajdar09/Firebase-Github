bd154e5f2bcb41bc450ba4d66725c168d4ad885a
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-06 16:51:20 +0530

    routing issues fixed👍

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./page.css";
 
 const PageNotFound = () => {
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(() => {
+      history.push("/signin");
+    }, 1000);
+  }, []);
   return (
-    <div style={{ background: " #ffffff" }}>
-      <h1>PageNotFound page</h1>
+    <div className="not-found">
+      <h1>Oooopss...</h1>
+      <h2>That page cannot be found.</h2>
+      <p>
+        Go back to the <Link href="/signin">Signin</Link>
+      </p>
     </div>
   );
 };
