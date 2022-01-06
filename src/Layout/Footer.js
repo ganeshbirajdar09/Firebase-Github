ea9c1e4184b58ce23a9b7915e11b1f740721012a
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-06 16:29:45 +0530

    footer changes👍

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -7,7 +7,14 @@ const Footer = () => {
       fluid
       tag="footer"
       className="position-fixed  text-center text-white text-uppercase fixed-bottom p-3"
-      style={{ background: "#333", fontWeight: "500" }}
+      style={{
+        background: "#333",
+        fontWeight: "500",
+        left: "0px",
+        bottom: "0px",
+        zIndex: "1",
+        width: "100%",
+      }}
     >
       Ganesh github app with firebase
     </Container>
